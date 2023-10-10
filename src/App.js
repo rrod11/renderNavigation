b7561b6acda4ea07de8ba5bc11aa11c649842161
Roderick <Roderickj800@gmail.com>
2023-10-10 18:52:29 -0400

    devious

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
-import { Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
-import Stocks from './components/Stocks';
-import Movies from './components/Movies';
+import { Route, Switch, Link, NavLink } from "react-router-dom";
+import Home from "./components/Home";
+import Stocks from "./components/Stocks";
+import Movies from "./components/Movies";
 
 function App() {
   return (
-    <div className='main'>
+    <div className="main">
       <h1>App Component</h1>
+      <nav className="comp nav">
+        <ul>
+          {/* <li>
+            <a href="/movies">Movies</a>
+          </li> */}
+          <li>
+            <NavLink
+              exact
+              to="/"
+              activeClassName="purple"
+              activeStyle={{ fontWeight: "bold" }}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/stocks"
+              activeClassName="purple"
+              activeStyle={{ fontWeight: "bold" }}
+            >
+              Stocks
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/movies"
+              activeClassName="purple"
+              activeStyle={{ fontWeight: "bold" }}
+            >
+              Movies
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
       <Switch>
-        <Route exact path='/'>
+        <Route exact path="/">
           <Home />
         </Route>
-        <Route path='/stocks'>
+        <Route path="/stocks">
           <Stocks />
         </Route>
-        <Route path='/movies'>
+        <Route path="/movies">
           <Movies />
+        </Route>
+        <Route path="/not-logged-in">
+          <h1> You Must Be Logged In To Enter.</h1>
         </Route>
         <Route>
           <h1>Page Not Found</h1>
--- src/components/Stocks/index.js
+++ src/components/Stocks/index.js
@@ -1,7 +1,20 @@
+import { Redirect, useHistory } from "react-router-dom";
+
 function Stocks() {
+  let loggedIn = true;
+  const history = useHistory();
+  if (!loggedIn) {
+    return <Redirect to="/not-logged-in" />;
+  }
+  const handleClick = () => {
+    window.alert("Sending info to the DB!");
+    history.push("/");
+  };
+
   return (
-    <div className='comp orange'>
+    <div className="comp orange">
       <h1>Stocks Component</h1>
+      <button onClick={handleClick}>Home</button>
     </div>
   );
 }
